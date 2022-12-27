5ed4e3fc73b87f1f54bf52df9689de03321dce64
Chris Northwood <chris@pling.org.uk>
2022-12-27 15:49:35 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container my-4">
+      <progress className="progress is-large">Loading</progress>
+    </div>
+  );
+}
